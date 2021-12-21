05ac00032ada85ce6025c8948a9d212a56281bb2
Kainatbatool040 <kainatbatool902@gmail.com>
2021-12-21 15:34:22 -0800

    Database connection
    successful Database connection

---
+++ simple_firebase/lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp=Firebase.initializeApp();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+            future: _fbApp,
+            builder: (Context,snapshot){
+              if(snapshot.hasError){
+                print("You have an error!${snapshot.error.toString()}");
+                return Text('Something went wrong');
+              }
+              else if(snapshot.hasData){
+                return MyHomePage(title: 'Flutter Counter App',);
+              }
+              else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }
+        )
+
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    DatabaseReference _testRef= FirebaseDatabase.instance.reference().child("Test");
+    _testRef.set("Hello World ${Random().nextInt(100)}");
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
